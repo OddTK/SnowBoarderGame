16af5e92b46eaa9a2c607f18acd459a294f464f8
Jesus <jesusbalandra@outlook.com>
2022-05-09 12:32:28 -0600

    started dust trail

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,25 +7,50 @@ public class PlayerController : MonoBehaviour
 {
     // adding a SerializeField meaning this variable can be contolled within Unity
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
     // creating global variable
     // accessing Rigidbody2D
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        RespondToBoost();
         // if the input key is the left arrow key then applying torque
         // i.e. a force that rotates our character when pressing the left arrow key
-        if(Input.GetKey(KeyCode.A))
+        RotatePlayer();
+    }
+
+    void RespondToBoost()
+    {
+        // if we push Up, then speed up
+        // otherwise stay at normal speed
+        // access the surfaceEffector2D and change its speed
+        if (Input.GetKey(KeyCode.W))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.A))
         {
             rb2d.AddTorque(torqueAmount);
         }
-        else if(Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             rb2d.AddTorque(-torqueAmount);
         }
