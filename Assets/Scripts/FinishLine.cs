25fd2ab96b123b7e911cae0dd2c0584fc8e892f3
Jesus <jesusbalandra@outlook.com>
2022-05-09 09:43:08 -0600

    added Invoke for scene delay with serializedField

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -5,12 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float sceneDelay = 1f;
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
-            // here we are using SceneManager to allow us to load our level1 scene once the player collides with our end pole
-            SceneManager.LoadScene(0);
+            // Invoke takes 2 parameters: ("name of method", delay)
+            Invoke("ReloadScene", sceneDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+         // here we are using SceneManager to allow us to load our level1 scene once the player collides with our end pole
+            SceneManager.LoadScene(0);
     }
 }
